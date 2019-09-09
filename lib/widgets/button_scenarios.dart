50fca2486fc6aca05efe635ca1ca467333769004
praetorian <filippov.a@e154.ru>
2019-09-09 14:15:22 +0700

    first commit

--- lib/widgets/button_scenarios.dart
+++ lib/widgets/button_scenarios.dart
@@ -17,21 +17,24 @@ class _ButtonScenarios extends State<ButtonScenarios> {
 
   double _width = 120;
   double _height = 120;
-  EdgeInsets _padding = EdgeInsets.all(5);
+  double _padding = 5;
+  double _squareScale = 1;
 
   _onTapDown(TapDownDetails details) {
     setState(() {
-      _width = 110;
-      _height = 110;
-      _padding = EdgeInsets.all(10);
+//      _width = 110;
+//      _height = 110;
+//      _padding = 10;
+      _squareScale = 0.95;
     });
   }
 
   _onTapCancel() {
     setState(() {
-      _width = 120;
-      _height = 120;
-      _padding = EdgeInsets.all(5);
+//      _width = 120;
+//      _height = 120;
+//      _padding = 5;
+      _squareScale = 1;
     });
   }
 
@@ -49,14 +52,15 @@ class _ButtonScenarios extends State<ButtonScenarios> {
         child: Container(
           constraints: BoxConstraints.expand(width: 130, height: 130),
           child: AnimatedContainer(
-            duration: Duration(microseconds: 100000),
-            reverseDuration: Duration(microseconds: 600),
+            duration: Duration(seconds: 1),
             curve: Curves.easeInOut,
-            padding: _padding,
-            child: Container(
-                decoration: new BoxDecoration(
-                  color: widget.active ? Color.fromRGBO(43, 152, 240, 1) : Color.fromRGBO(234, 235, 235, 1),
-                  borderRadius: new BorderRadius.all(const Radius.circular(12.0)),
+            padding: EdgeInsets.all(_padding),
+            child: Transform.scale(
+              scale: _squareScale,
+              child: Container(
+                  decoration: new BoxDecoration(
+                    color: widget.active ? Color.fromRGBO(43, 152, 240, 1) : Color.fromRGBO(234, 235, 235, 1),
+                    borderRadius: new BorderRadius.all(const Radius.circular(12.0)),
 //                boxShadow: [
 //                  new BoxShadow(
 //                    blurRadius: 3,
@@ -65,24 +69,25 @@ class _ButtonScenarios extends State<ButtonScenarios> {
 //                    offset: new Offset(3, 3),
 //                  )
 //                ],
-                ),
-                constraints: BoxConstraints.expand(width: _width, height: _height),
-                child: Container(
-                  padding: _padding,
-                  child: Center(
-                      child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        widget.name,
-                        style: TextStyle(
-                            color: widget.active ? Colors.white : Color.fromRGBO(97, 97, 97, 1),
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ],
+                  ),
+                  constraints: BoxConstraints.expand(width: _width, height: _height),
+                  child: Container(
+                    padding: EdgeInsets.all(_padding),
+                    child: Center(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          widget.name,
+                          style: TextStyle(
+                              color: widget.active ? Colors.white : Color.fromRGBO(97, 97, 97, 1),
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    )),
                   )),
-                )),
+            ),
           ),
         ));
     ;
